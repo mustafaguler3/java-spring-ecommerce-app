4d4e60d467c6a93779660dbc2e4b3f62c7e7b761
mustafa.glr <mustafa.glr@hotmail.com>
2026-03-29 12:39:13 +0300

    Add docker-compose.dev.yml, docker-compose.prod.yml and CI workflow

--- src/main/java/com/example/thymeleaf_demo/util/WishlistUtils.java
+++ src/main/java/com/example/thymeleaf_demo/util/WishlistUtils.java
@@ -4,11 +4,11 @@ import com.example.thymeleaf_demo.domain.Wishlist;
 
 import java.util.List;
 
-/*public class WishlistUtils {
+public class WishlistUtils {
     public static boolean isProductInWishlist(List<Wishlist> wishlist,Long productId){
         if (wishlist == null){
             return false;
         }
         return wishlist.stream().anyMatch(item -> item.getProduct().getId().equals(productId));
     }
-} */
+}
